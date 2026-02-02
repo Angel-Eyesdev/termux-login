20d54e29564ace2de8f95676e16161d018cd9c66
Angel-Eyesdev <kara.angeleyes@gmail.com>
2026-02-02 03:38:18 +0300

    All-in-one setup with password 123

--- setup.sh
+++ setup.sh
@@ -1,23 +1,32 @@
+#!/bin/bash
+# Update system
+pkg update && pkg upgrade -y
+pkg install figlet git python -y
+
+# Setup password file (.bashrc)
+cat << 'EOF' > ~/.bashrc
 clear
 figlet -f slant "PASHA"
 echo -e "\e[1;36m======================================\e[0m"
 echo -e "\e[1;33m       WELCOME TO PASHA TERMINAL      \e[0m"
 echo -e "\e[1;36m======================================\e[0m"
 
-# --- LOGIN SYSTEM ---
-PASSWORD="123" # لێرە پاسۆردەکەت بگۆڕە
-read -sp "[?] Enter Password: " input_pass
-echo -e "\n"
+PASSWORD="123"
+echo -n "[?] Enter Password: "
+read -s input_pass
+echo ""
 
-if [ "$input_pass" != "$PASSWORD" ]; then
-    echo -e "\e[1;31m[!] Access Denied!\e[0m"
+if [ "$input_pass" = "$PASSWORD" ]; then
+    echo -e "\n\e[1;32m[+] Access Granted! Welcome.\e[0m\n"
+else
+    echo -e "\n\e[1;31m[!] Wrong Password. Closing...\e[0m"
+    sleep 2
     exit
 fi
 
-clear
-figlet -f slant "PASHA"
-# Aliases
-alias get='yt-dlp -o "~/storage/downloads/%(title)s.%(ext)s"'
-alias kurd='trans -t ckb'
 alias clean='rm -rf ~/.bash_history && clear && figlet -f slant "PASHA"'
+alias get='yt-dlp -o "~/storage/downloads/%(title)s.%(ext)s"'
+EOF
+
+echo "Done! Restart Termux now."
 
